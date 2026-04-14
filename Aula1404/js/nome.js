b32938e91109fe9d2b2e17301f65d372546deb71
DenisCiriaco <denis.sorban@gmail.com>
2026-04-14 08:08:25 -0300

    Add files via upload

---
+++ Aula1404/js/nome.js
@@ -0,0 +1,9 @@
+var nome = "Thiago";
+var nome2 = prompt("Digite seu nome: ");
+var idade = parseInt(prompt("Digite sua Idade: "));
+
+
+console.log("Seu nome é:", nome);
+console.log("Seu idade é:", idade);
+alert("Seu nome é: "+ nome2);
+alert("Sua idade é: "+ idade);
---
+++ Aula1404/js/soma.js
@@ -0,0 +1,13 @@
+var numero1 = parseFloat(prompt("Digite um número para operação: "));
+var numero2 = parseFloat(prompt("Digite outro número para operação: "));
+var soma = numero1 + numero2;
+var divisao = numero1 / numero2;
+var subtracao = numero1 - numero2;
+var multiplicacao = numero1 * numero2;
+
+
+console.log("Soma dos números: ", soma);
+console.log("Subtração dos números: ", subtracao);
+console.log("Multiplicação dos números: ", multiplicacao);
+console.log("Divisão dos números: ", divisao);
+
