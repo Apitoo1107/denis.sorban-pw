f7febf5c0f04c92b83f75d22d797fbf17881b7aa
DenisCiriaco <denis.sorban@gmail.com>
2026-05-05 08:08:47 -0300

    Add files via upload

---
+++ Estrutura de repetição/js/vetor2.js
@@ -0,0 +1,7 @@
+var palavra = "Denis Lindo";
+var i = 0
+while(i < 11){
+    
+    console.log(palavra[i])
+    i++
+}
---
+++ Estrutura de repetição/js/while.js
@@ -0,0 +1,10 @@
+var x = 0
+var soma = 0
+
+while(x < 5){
+    x++
+    soma += x
+    console.log(soma)
+    console.log("testando repetição"+ x)
+
+}
